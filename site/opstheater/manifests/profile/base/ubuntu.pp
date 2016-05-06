c514757ad4e399936c3955b4bb27c7ef413f8b91
shreejit13 <shreejit13@gmail.com>
2016-05-06 13:17:57 +0530

    fixed indentation

--- site/opstheater/manifests/profile/base/ubuntu.pp
+++ site/opstheater/manifests/profile/base/ubuntu.pp
@@ -38,20 +38,20 @@ class opstheater::profile::base::ubuntu {
   }
 
 
-  icinga2::Object::Host {
-    display_name    => $::fqdn,
-    check_command   => 'cluster-zone',
-    target_dir      => '/etc/icinga2/objects/hosts',
-    target_file_name=> "${::fqdn}.conf",
-  }
+#  icinga2::Object::Host {
+#    display_name    => $::fqdn,
+#    check_command   => 'cluster-zone',
+#    target_dir      => '/etc/icinga2/objects/hosts',
+#    target_file_name=> "${::fqdn}.conf",
+#  }
 
-  @@icinga2::object::host { $::fqdn:
-    ipv4_address => $::ipaddress,
-    vars         => {
-      os              => 'Linux',
-      remote          => true,
-      remote_endpoint => $::fqdn
-    },
-  }
+#  @@icinga2::object::host { $::fqdn:
+#    ipv4_address => $::ipaddress,
+#    vars         => {
+#      os              => 'Linux',
+#      remote          => true,
+#      remote_endpoint => $::fqdn
+#    },
+#  }
 
 }
