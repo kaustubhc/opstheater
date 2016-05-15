1368d7afa585b01d09cce343043c81e86938f736
Walter Heck <walterheck@olindata.com>
2016-05-15 23:37:30 +0200

    Fix elastic datasource api calls, there were some missing parameters

--- site/opstheater/manifests/profile/grafana.pp
+++ site/opstheater/manifests/profile/grafana.pp
@@ -10,10 +10,13 @@ class opstheater::profile::grafana {
     install_method => $install_method,
   }
 
+  # curl http://admin:admin@localhost:3000/api/datasources -X POST -H 'Content-Type: application/json;charset=UTF-8' \
+  # --data '{"Name":"OpsTheater7","type":"elasticsearch","url":"http://elasticsearch.opstheater.vm:9200","access":"proxy","isDefault":true,"database":"[logstash-]YYYY.MM.DD","jsonData":{"timeField":"@timestamp", "esVersion": 2, "interval":"Daily"}}'
+
   exec { 'elastic_datasource':
     path    => '/bin:/usr/bin:/sbin:/usr/sbin',
     unless  => "[ `curl --silent 'http://${grafanauser}:${grafanapasswd}@${grafanaurl}:3000/api/datasources' -X GET | tr ',' '\\n' | grep '\"name\":\"Elastic\"'` == '\"name\":\"Elastic\"' ]",
-    command => "sleep 10;curl 'http://${grafanauser}:${grafanapasswd}@${grafanaurl}:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8\' --data-binary \'{\"name\":\"Elastic\",\"type\":\"elasticsearch\",\"url\":\"http://${grafanaurl}:9200\",\"access\":\"proxy\",\"isDefault\":true,\"database\":\"[logstash-]YYY.MM.DD\"'}",
+    command => "sleep 10;curl 'http://${grafanauser}:${grafanapasswd}@${grafanaurl}:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{\"name\":\"Elastic\",\"type\":\"elasticsearch\",\"url\":\"http://${grafanaurl}:9200\",\"access\":\"proxy\",\"isDefault\":true,\"database\":\"[logstash-]YYYY.MM.DD\",\"jsonData\":{\"timeField\":\"@timestamp\", \"esVersion\": 2, \"interval\":\"Daily\"}'}",
     require => Class[ 'grafana' ],
   }
 
